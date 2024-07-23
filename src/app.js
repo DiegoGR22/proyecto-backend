0574533843f7b3a3cfe9210a284bdbc78da2800c
DiegoGR22 <guerrero-romero-diego@hotmail.com>
2024-07-23 13:42:37 -0600

    Config handlebars y websocket

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import { __dirname } from './utils.js';
 import ProductRouter from './routes/product.router.js';
 import CartRouter from './routes/cart.router.js';
+import ViewsRouter from './routes/views.routes.js';
+import { Server } from 'socket.io'
 
 const app = express();
+const PORT = 8080 || 3000;
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', ViewsRouter);
 app.use('/api/products', ProductRouter);
 app.use('/api/carts', CartRouter);
 
 
-
-app.listen(8080, () => {
+const httpServer = app.listen(PORT, () => {
     console.log("Server listening on")
 });
+
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+    console.log("New connection established")
+})
